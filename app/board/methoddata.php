e0cf252da7bd460466de5b2a214282ddf85969d3
Allen Chou <s3131212@gmail.com>
2015-01-20 21:43:13 +0800

    學生部分模組化完成

---
+++ app/board/methoddata.php
@@ -0,0 +1,14 @@
+<?php
+$GLOBALS["methoddata"] = array(
+    "student"=>array(
+        "list"=>array("methodname"=>"list_board","parameter"=>array($_SESSION["class"])),
+        "view"=>array("methodname"=>"view_board","parameter"=>array($_GET["id"])),
+        "new"=>array("methodname"=>"new_board","parameter"=>"")
+    ),
+    "teacher"=>array(
+
+    ),
+    "staff"=>array(
+
+    )
+);
---
+++ app/user/methoddata.php
@@ -0,0 +1,12 @@
+<?php
+$GLOBALS["methoddata"] = array(
+    "student"=>array(
+        "view"=>array("methodname"=>"get_data","parameter"=>array($_SESSION["login_id"]))
+    ),
+    "teacher"=>array(
+
+    ),
+    "staff"=>array(
+
+    )
+);
